52fcdab209c048f7d0c60705d56c100b59c6362e
Akshay <akshay.maniar2000@gmail.com>
2022-03-15 15:51:36 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# weather-app
+
+It is a simple webpage where you can get weather information, based on the city you enter. The features include the current temperature, minimum temperature of today, maximum temperature of today, wind speed, air pressure of today, as well as the temperature upcoming five days with weather state name.
+
+## Screenshot
+
+![ScreenShot](./screenshots/screenshot.png)
+
+## [Live Demo](https://AkshayManiar.github.io/weather-app/)
+
+## Getting Started
+
+First of all you need an API from [metaweather](https://www.metaweather.com/api/), you can get it by creating an account on their website or you can use it for free by searching weather api on their website. After you get the API fetch the data from it and convert it into JSON file and use the data as per your convenience.
+
+### Note
+
+If you run it on your local server than you might get an error like this \/
+
+`Access to fetch at 'https://www.metaweather.com/api/location/search/?query=Mumbai' from origin 'http://127.0.0.1:5500' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource. If an opaque response serves your needs, set the request's mode to 'no-cors' to fetch the resource with CORS disabled.`
+
+_It because If you don’t control the server your frontend code is sending a request to, and the problem with the response from that server is just the lack of the necessary Access-Control-Allow-Origin header, you can still get things to work—by making the request through a CORS proxy._
+**Solution:**
+Download an Extension in Google Chrome - [Moesif Origin & CORS Changer](https://chrome.google.com/webstore/detail/moesif-origin-cors-change/digfbfaphojjndkpccljibejjbppifbc).
+This plugin allows you to send cross-domain requests. You can also override Request Origin and CORS headers.
+
+## Credits
+
+- [metaweather](https://www.metaweather.com/api/)(Weather data API)
+- [Commercial Type & Greg Gazdowicz](https://fonts.google.com/specimen/Roboto+Serif?query=roboto+ser)(Fonts)
---
+++ js/script.js
@@ -0,0 +1,132 @@
+const APIURL = "https://www.metaweather.com/api/location/search/?query=";
+const WOEIDURL = "https://www.metaweather.com/api/location/";
+const IMGURL = "https://www.metaweather.com/static/img/weather/png/64/";
+
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+
+async function getWoeid(city) {
+  const resp = await fetch(APIURL + city);
+  const respData = await resp.json();
+  const cts = JSON.stringify(respData);
+  if (5 < cts.length && cts.length < 200) {
+    const dfnl = JSON.parse(cts.slice(1, -1));
+    getWeather(dfnl.woeid);
+  } else {
+    errorDisplay();
+  }
+}
+
+async function getWeather(woeID) {
+  const resp = await fetch(WOEIDURL + woeID + "/");
+  const respData = await resp.json();
+  displayWeather(respData);
+}
+
+function displayWeather(data) {
+  const weather = document.createElement("div");
+  weather.classList.add("weather");
+  weather.innerHTML = `
+  <div class="today">
+    <div class="sub-today">
+      <img src="${
+        IMGURL + data.consolidated_weather[0].weather_state_abbr
+      }.png"/>
+      <h3>${data.consolidated_weather[0].weather_state_name}</h3>
+    </div>
+    <div class="sub-today">
+      <h1>${Math.floor(data.consolidated_weather[0].the_temp)}°C</h1>
+      <p>min-temp: ${Math.floor(data.consolidated_weather[0].min_temp)}°C</p>
+      <p>max-temp: ${Math.floor(data.consolidated_weather[0].max_temp)}°C</p>
+      <p>wind: ${data.consolidated_weather[0].wind_speed.toFixed(2)}Kmph</p>
+      <p>Pressure: ${data.consolidated_weather[0].air_pressure}mb</p>
+    </div>
+  </div>
+  <div class="future">
+    <div class="sub-future">
+      <h5>${data.consolidated_weather[1].applicable_date
+        .split("-")
+        .reverse()
+        .join("/")
+        .slice(0, -5)}</h5>
+      <img src="${
+        IMGURL + data.consolidated_weather[1].weather_state_abbr
+      }.png "/>
+      <p>${Math.floor(data.consolidated_weather[1].the_temp)}°C 
+      </p>
+      <p>${data.consolidated_weather[1].weather_state_name}</p>
+    </div>
+    <div class="sub-future">
+      <h5>${data.consolidated_weather[2].applicable_date
+        .split("-")
+        .reverse()
+        .join("/")
+        .slice(0, -5)}</h5>
+      <img src="${
+        IMGURL + data.consolidated_weather[2].weather_state_abbr
+      }.png "/>
+      <p>${Math.floor(data.consolidated_weather[2].the_temp)}°C 
+      </p>
+      <p>${data.consolidated_weather[2].weather_state_name}</p>
+    </div>
+    <div class="sub-future">
+      <h5>${data.consolidated_weather[3].applicable_date
+        .split("-")
+        .reverse()
+        .join("/")
+        .slice(0, -5)}</h5>
+      <img src="${
+        IMGURL + data.consolidated_weather[3].weather_state_abbr
+      }.png "/>
+      <p>${Math.floor(data.consolidated_weather[3].the_temp)}°C 
+      </p>
+      <p>${data.consolidated_weather[3].weather_state_name}</p>
+    </div>
+    <div class="sub-future">
+      <h5>${data.consolidated_weather[4].applicable_date
+        .split("-")
+        .reverse()
+        .join("/")
+        .slice(0, -5)}</h5>
+      <img src="${
+        IMGURL + data.consolidated_weather[4].weather_state_abbr
+      }.png "/>
+      <p>${Math.floor(data.consolidated_weather[4].the_temp)}°C 
+      </p>
+      <p>${data.consolidated_weather[4].weather_state_name}</p>
+    </div>
+    <div class="sub-future">
+      <h5>${data.consolidated_weather[5].applicable_date
+        .split("-")
+        .reverse()
+        .join("/")
+        .slice(0, -5)}</h5>
+      <img src="${
+        IMGURL + data.consolidated_weather[5].weather_state_abbr
+      }.png "/>
+      <p>${Math.floor(data.consolidated_weather[5].the_temp)}°C 
+      </p>
+      <p>${data.consolidated_weather[5].weather_state_name}</p>
+    </div>
+  </div>
+  `;
+  main.innerHTML = "";
+  main.appendChild(weather);
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const city = search.value;
+  if (city) {
+    getWoeid(city);
+  } else {
+    main.innerHTML = "";
+    main.innerHTML = `<h3 class="error_message">Please enter a valid Input</h3>`;
+  }
+});
+
+function errorDisplay() {
+  main.innerHTML = "";
+  main.innerHTML = `<h3 class="error_message">Sorry data not available</h3>`;
+}
